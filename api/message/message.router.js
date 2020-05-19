e7f526a6157a49a7cb8d43f07fdf38d6ce0493cd
LeanderLoomans <s1115153@student.hsleiden.nl>
2020-05-19 11:24:30 +0200

    Added authentication & user-roles to backend

--- api/message/message.router.js
+++
@@ -1,5 +0,0 @@
-const { getUserMessages } = require("./message.controller");
-const router = require("express").Router();
-const { checkToken } = require("../../auth/token_validation");
-
-router.get("/:userID", checkToken, getUserMessages);
