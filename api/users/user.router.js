a65912229973e46b02775706f7454a14d51bfc66
Fillette97 <48556597+Fillette97@users.noreply.github.com>
2020-05-26 15:22:00 +0200

    imports na mergen met master goed gezet

--- api/users/user.router.js
+++ api/users/user.router.js
@@ -1,4 +1,4 @@
-const { createUser, getUserById, getUsers, updateUsers, deleteUser, login } = require("./user.controller");
+const { createUser, getUserById, getUsers, updateUsers, deleteUser, login, getInactiveUsers,activateUser } = require("./user.controller");
 const router = require("express").Router();
 
 const { checkToken } = require("../../auth/token_validation");
