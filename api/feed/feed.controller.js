248f8573c4b47572efc2e82aa65701e908621d17
Fillette97 <48556597+Fillette97@users.noreply.github.com>
2020-06-11 10:14:58 +0200

    backend niet werkend

--- api/feed/feed.controller.js
+++ api/feed/feed.controller.js
@@ -1,21 +1,27 @@
 const {insertPost} = require("./feed.service");
 const{getCategories} = require("./feed.service");
 
+const{getUserIdFromToken} = require("../../auth/token_validation");
+
+
 module.exports = {
+
+
     addPost:(req,res) => {
+        const userId = getUserIdFromToken(req.get("authorization"))
         console.log("got to backend")
         const body = req.body;
-        insertPost(body, (err, results) => {
+        insertPost(body, userId,(err, results) => {
             if (err) {
                 console.log(err);
-                return res.status(500).json({
+                return ({
                     success: 0,
                     message: "Database connection error"
                 });
             }
-            return res.status(200).json({
+            return res.json({
                 success: 1,
-                data: results
+                message: "data succesvol toegevoegd"
             })
         });
     },
--- api/feed/feed.service.js
+++ api/feed/feed.service.js
@@ -2,32 +2,32 @@ const pool = require("../../config/database");
 
 
 module.exports = {
-    insertPost: (data, callback) => {
+    insertPost: (data, userId, callback) => {
+console.log("Dit is de data" + data)
         pool.query(
             // `insert into Post(text, userId, postDate, categoryId, title)
             //     values(?, ?,now(),?,?)`,
-            `insert into Post(text, image, userId, categoryId, title)
-                values(?, FROM_BASE64(?), ?, ?, ?)`,
+            `insert into Post(text, image, userid, categoryId, title,postDate)
+                values(?, FROM_BASE64(?), ?, ?, ?, now())`,
             [
                 data.text,
                 data.image,
-                data.userId,
+                userId,
                 data.categoryId,
                 data.title,
-
             ],
+            console.log("inserted"),
             (error, results, fields) => {
                 if (error) {
-                    console.log(error);
                     return callback(error);
                 }
                 return callback(null, results);
             }
         )
-},
+    },
 
     getCategories: callback => {
-        console.log("CATEGORIES")
+        // console.log("CATEGORIES")
         pool.query(
             `select * from Category`,
             [],
@@ -35,7 +35,7 @@ module.exports = {
                 if (error) {
                     return callback(error);
                 }
-                console.log(results[0].categoryId)
+                // console.log(results[0].categoryId)
                 return callback(null, results);
             }
         )
@@ -43,23 +43,23 @@ module.exports = {
 
 
     create: (data, callback) => {
-    pool.query(
-    `insert into Account(firstname, lastname, register_date, password, role, email)
+        pool.query(
+            `insert into Account(firstname, lastname, register_date, password, role, email)
         values(?, ?, ?, ?, ?, ?)`,
-    [
-    data.first_name,
-    data.last_name,
-    data.register_date,
-    data.password,
-    data.role,
-    data.email,
-    ],
-    (error, results, fields) => {
-    if (error) {
-        return callback(error);
-    }
-    return callback(null, results);
-    }
-    )
+            [
+                data.first_name,
+                data.last_name,
+                data.register_date,
+                data.password,
+                data.role,
+                data.email,
+            ],
+            (error, results, fields) => {
+                if (error) {
+                    return callback(error);
+                }
+                return callback(null, results);
+            }
+        )
     }
 }
