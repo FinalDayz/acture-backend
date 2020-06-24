8b783607b243c2a3ed502d6f7339109a31b34784
LeanderLoomans <s1115153@student.hsleiden.nl>
2020-06-24 10:41:03 +0200

    Startups toevoegen

--- api/startups/startup.router.js
+++ api/startups/startup.router.js
@@ -1,12 +1,12 @@
-const {followOrNot} = require("./startup.controller");
-const { followQuery, workingForQuery } = require("./startup.controller");
+const { followOrNot, followQuery, workingForQuery, addStartup } = require("./startup.controller");
 const router = require("express").Router();
-
 const { checkToken } = require("../../auth/token_validation");
+const { checkRole } = require("../../auth/role_validation");
 
 router.get("/byuserid/:userid", checkToken, followQuery);
 router.get("/bystartupid/:startupid", checkToken, workingForQuery);
 router.patch("/:id/:followOrNot", checkToken, followOrNot);
+router.patch("/new", checkToken, checkRole('member'), addStartup);
+
 
 module.exports = router;
-"/active/:active"
